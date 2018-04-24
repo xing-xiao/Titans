2610ecd491b79b339dbe4e0ef9545d06ad3f916e
012503 <xingxiao@htsc.com>
2018-04-24 16:48:29 +0800

    restfull

--- deployment/docker/restfull/docker-entrypoint.sh
+++ deployment/docker/restfull/docker-entrypoint.sh
@@ -5,6 +5,6 @@ JAR_PATH=/run/jars/
 sleep 30
 
 curl -XPOST ${FLINK_SERVER}jars/upload -H "Expect:" -F "jarfile=@${JAR_PATH}TsapCEPEngine-assembly-0.1.jar" > ${JAR_PATH}jar.id
-
-python3 run.py
+tailf ${JAR_PATH}jar.id
+# python3 run.py
 
